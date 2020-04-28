00d7077514d4148482e5cd0e82182288a9e7334b
Megan Kratz <meganbkratz@gmail.com>
2020-04-28 13:26:08 -0600

    removed __main__ code

--- aisynphys/ui/pair_analysis/pair_analysis.py
+++ aisynphys/ui/pair_analysis/pair_analysis.py
@@ -190,7 +190,7 @@ class ExperimentSelector(pg.QtGui.QWidget):
         layout = pg.QtGui.QVBoxLayout()
         layout.setContentsMargins(3,3,3,3)
         self.setLayout(layout)
-        
+
         self.select_ptree = ptree.ParameterTree(showHeader=False)
         self.hash_select = Parameter.create(name='Hashtags', type='group', children=
             [{'name': 'With multiple selected:', 'type': 'list', 'values': ['Include if any appear', 'Include if all appear'], 'value': 'Include if any appear'}]+
@@ -856,41 +856,3 @@ class PairAnalysis(object):
                     if mode == 'ic':
                         self.ic_plot.plot_fit(fit_tseries, holding, fit_pass=fit_pass)            
 
-
-
-if __name__ == '__main__':
-    app = pg.mkQApp()
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--timestamps', type=str, nargs='*')
-    parser.add_argument('--dbg', default=False, action='store_true')
-    parser.add_argument('expt_id', type=str, nargs='?', default=None)
-    parser.add_argument('pre_cell_id', type=str, nargs='?', default=None)
-    parser.add_argument('post_cell_id', type=str, nargs='?', default=None)
-
-    args = parser.parse_args(sys.argv[1:])
-
-    if args.dbg:
-        pg.dbg()
-
-    default_session = db.session()
-    notes_session = notes_db.db.session()
-    # timestamps = [r.acq_timestamp for r in db.query(db.Experiment.acq_timestamp).all()]
-    timestamps = []
-    
-    mw = MainWindow(default_session, notes_session)
-    if args.timestamps is not None:
-        timestamps = args.timestamps
-    elif args.expt_id is not None:
-        timestamps = [args.expt_id]
-    
-    q = default_session.query(db.Experiment).filter(db.Experiment.acq_timestamp.in_(timestamps))
-    expts = q.all()
-    mw.set_expts(expts)
-
-    if None not in (args.expt_id, args.pre_cell_id, args.post_cell_id):
-        expt = db.experiment_from_ext_id(args.expt_id)
-        pair = expt.pairs[args.pre_cell_id, args.post_cell_id]
-        mw.experiment_browser.select_pair(pair.id)
-
-    if sys.flags.interactive == 0:
-        app.exec_()
