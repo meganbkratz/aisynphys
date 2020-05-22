139fe4a95e5fe4b4ee75385990af667bf2f00953
Megan Kratz <meganbkratz@gmail.com>
2020-05-22 14:16:38 -0600

    rearrange gui - work on fitting got user latency adjustment working

--- aisynphys/ui/pair_analysis/pair_analysis_opto.py
+++ aisynphys/ui/pair_analysis/pair_analysis_opto.py
@@ -1,9 +1,13 @@
 import datetime
 import pyqtgraph as pg
+import numpy as np
 from collections import OrderedDict
 
 from neuroanalysis.ui.plot_grid import PlotGrid
-from neuroanalysis.fitting import fit_psp
+from neuroanalysis.fitting.psp import fit_psp, Psp
+import neuroanalysis.filter as filters
+import neuroanalysis.event_detection as ev_detect
+from neuroanalysis.data.dataset import TSeries
 
 from aisynphys.ui.pair_analysis.pair_analysis import ControlPanel, SuperLine
 from aisynphys.ui.experiment_selector import ExperimentSelector
@@ -32,11 +36,18 @@ class OptoPairAnalysisWindow(pg.QtGui.QWidget):
 
         self.h_splitter = pg.QtGui.QSplitter(pg.QtCore.Qt.Horizontal)
 
+        self.selectTabWidget = pg.QtGui.QTabWidget()
+        self.analyzerTabWidget = pg.QtGui.QTabWidget()
+        self.analyzerTabWidget.addTab(ResponseAnalyzer(self), "Responses")
+
         self.expt_selector = ExperimentSelector(default_session, notes_session, hashtags=[])
         self.expt_browser = ExperimentBrowser()
         self.ctrl_panel = ControlPanel()
         self.plot_grid = PlotGrid()
+        #self.plot_grid = FitPlotter()
         self.latency_superline = SuperLine()
+
+        self.selectTabWidget.addTab(self.expt_selector, 'Experiment Select')
 
         self.ptree = pg.parametertree.ParameterTree(showHeader=False)
         self.pair_param = pg.parametertree.Parameter.create(name='Current Pair', type='str', readonly=True)
@@ -54,9 +65,13 @@ class OptoPairAnalysisWindow(pg.QtGui.QWidget):
         for widget in [self.expt_browser, self.ptree, self.fit_btn, self.fit_ptree, self.save_btn]:
             v_splitter.addWidget(widget)
 
-        self.h_splitter.addWidget(self.expt_selector)
-        self.h_splitter.addWidget(v_splitter)
-        self.h_splitter.addWidget(self.plot_grid)
+        self.selectTabWidget.addTab(v_splitter, "Pair Select")
+
+        #self.h_splitter.addWidget(self.expt_selector)
+        #self.h_splitter.addWidget(v_splitter)
+        self.h_splitter.addWidget(self.selectTabWidget)
+        #self.h_splitter.addWidget(self.plot_grid)
+        self.h_splitter.addWidget(self.analyzerTabWidget)
 
         self.layout.addWidget(self.h_splitter)
         self.show()
@@ -80,8 +95,10 @@ class OptoPairAnalysisWindow(pg.QtGui.QWidget):
     def reset(self):
         self.pulse_responses = None
         self.sorted_responses = None
-        self.latency_superline.clear_lines()
-        self.plot_grid.clear()
+        self.analyzers = {}
+        #self.latency_superline.clear_lines()
+        #self.plot_grid.clear()
+        self.analyzerTabWidget.clear()
         self.ctrl_panel.fit_params.clearChildren()
         self.ctrl_panel.output_params.child('Comments', 'Hashtag').setValue('')
         self.ctrl_panel.output_params.child('Comments', '').setValue('')
@@ -150,44 +167,51 @@ class OptoPairAnalysisWindow(pg.QtGui.QWidget):
 
             ## create plots and parameter items for each catagory in sorted responses
             self.ctrl_panel.create_new_fit_params([str(k) for k in self.sorted_responses.keys()])
-            self.create_new_plots(self.sorted_responses.keys())
+            #self.create_new_plots(self.sorted_responses.keys())
+            self.create_new_analyzers(self.sorted_responses.keys())
             self.fit_params = {key:{'initial':{}, 'fit':{}} for key in self.sorted_responses.keys()}
 
             self.plot_responses()
 
-    def create_new_plots(self, categories):
-        self.plot_grid.set_shape(len(categories), 1)
+    # def create_new_plots(self, categories):
+    #     self.plot_grid.set_shape(len(categories), 1)
 
-        unit_map={'vc':'A', 'ic':'V'}
+    #     unit_map={'vc':'A', 'ic':'V'}
 
-        for i, key in enumerate(categories):
-            plot = self.plot_grid[(i,0)]
-            plot.setTitle(str(key))
-            plot.setLabel('left', text="%dmV holding" % key[1], units=unit_map[key[0]])
-            plot.setLabel('bottom', text='Time from stimulation', units='s')
-            plot.addItem(self.latency_superline.new_line(self.default_latency))
+    #     for i, key in enumerate(categories):
+    #         plot = self.plot_grid[(i,0)]
+    #         plot.setTitle(str(key))
+    #         plot.setLabel('left', text="%dmV holding" % key[1], units=unit_map[key[0]])
+    #         plot.setLabel('bottom', text='Time from stimulation', units='s')
+    #         plot.addItem(self.latency_superline.new_line(self.default_latency))
+
+    def create_new_analyzers(self, catagories):
+        for i, key in enumerate(catagories):
+            self.analyzers[key] = ResponseAnalyzer(self)
+            self.analyzerTabWidget.addTab(self.analyzers[key], str(key))
 
     def plot_responses(self):
-        qc_color = {'qc_pass': (255, 255, 255, 100), 'qc_fail': (255, 0, 0, 100)}
+        #qc_color = {'qc_pass': (255, 255, 255, 100), 'qc_fail': (255, 0, 0, 100)}
         
         for i, key in enumerate(self.sorted_responses.keys()):
-            for qc, prs in self.sorted_responses[key].items():
-                if len(prs) == 0:
-                    continue
-                prl = PulseResponseList(prs)
-                post_ts = prl.post_tseries(align='pulse', bsub=True)
+            self.analyzers[key].plot_responses(self.sorted_responses[key])
+            # for qc, prs in self.sorted_responses[key].items():
+            #     if len(prs) == 0:
+            #         continue
+            #     prl = PulseResponseList(prs)
+            #     post_ts = prl.post_tseries(align='pulse', bsub=True)
                 
-                for trace in post_ts:
-                    item = self.plot_grid[(i,0)].plot(trace.time_values, trace.data, pen=qc_color[qc])
-                    if qc == 'qc_fail':
-                        item.setZValue(-10)
-                    #self.items.append(item)
-                if qc == 'qc_pass':
-                    grand_trace = post_ts.mean()
-                    item = self.plot_grid[(i,0)].plot(grand_trace.time_values, grand_trace.data, pen={'color': 'b', 'width': 2})
-                    #self.items.append(item)
-            self.plot_grid[(i,0)].autoRange()
-            self.plot_grid[(i,0)].setXRange(-5e-3, 10e-3)
+            #     for trace in post_ts:
+            #         item = self.plot_grid[(i,0)].plot(trace.time_values, trace.data, pen=qc_color[qc])
+            #         if qc == 'qc_fail':
+            #             item.setZValue(-10)
+            #         #self.items.append(item)
+            #     if qc == 'qc_pass':
+            #         grand_trace = post_ts.mean()
+            #         item = self.plot_grid[(i,0)].plot(grand_trace.time_values, grand_trace.data, pen={'color': 'b', 'width': 2})
+            #         #self.items.append(item)
+            # self.plot_grid[(i,0)].autoRange()
+            # self.plot_grid[(i,0)].setXRange(-5e-3, 10e-3)
 
     def fit_responses(self):
         latency = self.ctrl_panel.user_params['User Latency']
@@ -205,24 +229,67 @@ class OptoPairAnalysisWindow(pg.QtGui.QWidget):
                     
                 tsl = PulseResponseList(prs).post_tseries(align='pulse', bsub=True)
                 average = tsl.mean()
-                fit = fit_psp(average, latency_window, clamp_mode, baseline_like_psp=True)
-                fit_ts = average.copy(data=fit.best_fit)
+                #fit = fit_psp(average, latency_window, clamp_mode, baseline_like_psp=True)
+                fits = self.fit_psps_opto(average, latency_window, clamp_mode, baseline_like_psp=True)
+                d = np.zeros(average.data.shape, average.data.dtype)
+                for j, fit in fits.items():
+                    v = fit.values
+                    fit_data = Psp.psp_func(average.time_values, v['xoffset'], v['yoffset'], v['rise_time'], v['decay_tau'], v['amp'], v['rise_power'])
+                    self.plot_grid[(i,0)].plot(average.time_values, fit_data, pen=pg.intColor(j, minHue=100))
+                    d += fit_data
+
+                self.plot_grid[(i,0)].plot(average.time_values, d, pen='r')
+
+
+                #fit_ts = average.copy(data=fit.best_fit)
                     
-                self.fit_params[key]['initial']['xoffset'] = latency
-                self.fit_params[key]['fit']['nrmse'] = fit.nrmse()
-                self.fit_params[key]['fit'].update(fit.best_values)
+                #self.fit_params[key]['initial']['xoffset'] = latency
+                #self.fit_params[key]['fit']['nrmse'] = fit.nrmse()
+                #self.fit_params[key]['fit'].update(fit.best_values)
 
-                fit_pass = fit.nrmse() < self.nrmse_threshold
-                self.ctrl_panel.output_params.child('Fit parameters', str(key), 'Fit Pass').setValue(fit_pass)
-                self.ctrl_panel.update_fit_param(key, self.fit_params[key]['fit'])
+                #fit_pass = fit.nrmse() < self.nrmse_threshold
+                #self.ctrl_panel.output_params.child('Fit parameters', str(key), 'Fit Pass').setValue(fit_pass)
+                #self.ctrl_panel.update_fit_param(key, self.fit_params[key]['fit'])
 
-                self.plot_grid[(i,0)].plot(fit_ts.time_values, fit_ts.data, pen={'color':fit_color[fit_pass], 'width': 3})
+                #self.plot_grid[(i,0)].plot(fit_ts.time_values, fit_ts.data, pen={'color':fit_color[fit_pass], 'width': 3})
                 
                 dlg += 1
                 if dlg.wasCanceled():
                     raise Exception("User canceled fit")
 
         self.generate_warnings()
+
+    def fit_psps_opto(self, average, latency_window, clamp_mode, baseline_like_psp=True):
+
+        tau=20e-3 ## for epsps, 50ms for ipsps
+
+        filtered = filters.bessel_filter(average, 4000, order=2, btype='low', bidir=True)
+        exp_dec = ev_detect.exp_deconvolve(filtered, tau)
+
+        threshold = 0.3*max(exp_dec.data)
+
+        ## index, length, sum, peak, peak_index, time, duration, area, peak_time
+        events = ev_detect.threshold_events(exp_dec, threshold)
+        sign=1
+        fits={}
+
+        for i, ev in enumerate(events):
+            window = [ev['time']-0.002, ev['time']+0.002]
+
+            if i == len(events)-1:
+                data = exp_dec.time_slice(ev['time']-0.0001, None)
+            else:
+                data = exp_dec.time_slice(ev['time']-0.0001, events[i+1]['time'])
+
+            d = np.concatenate([np.zeros(100), data.data, np.zeros(300)])
+            tv = np.concatenate([np.arange(data.t0-data.dt*100, data.t0-data.dt/10., data.dt), data.time_values, np.arange(data.time_values[-1]+data.dt, data.time_values[-1]+data.dt*301, data.dt)])
+            data = TSeries(data=d, time_values=tv)
+            data = ev_detect.exp_reconvolve(data, tau)
+            fits[i] = fit_psp(data, window, clamp_mode, sign, exp_baseline=False)
+
+        return fits
+
+
 
     def load_saved_fit(self, record):
         raise Exception('implement me!')
@@ -280,4 +347,185 @@ class OptoPairAnalysisWindow(pg.QtGui.QWidget):
                 raise Exception('Save Cancelled')
         session.close()
     
+
+class ResponseAnalyzer(pg.QtGui.QWidget):
+
+    def __init__(self, key=None):
+        pg.QtGui.QWidget.__init__(self)
+        layout = pg.QtGui.QHBoxLayout()
+        layout.setContentsMargins(0,0,0,0)
+        self.setLayout(layout)
+
+        h_splitter = pg.QtGui.QSplitter(pg.QtCore.Qt.Horizontal)
+        layout.addWidget(h_splitter)
+
+        self.responses = None ## holder for {pass:[tseries], fail:[tseries]}
+        self.average_response = None ## holder for the average response
+        self.key = key
+        self.param_tree = pg.parametertree.ParameterTree()
+        self.plot_grid=PlotGrid()
+        self.fit_btn = pg.QtGui.QPushButton("Fit responses")
+        self.add_btn = pg.QtGui.QPushButton("Add analysis")
+
+        v_widget = pg.QtGui.QWidget()
+        v_layout = pg.QtGui.QVBoxLayout()
+        
+        v_layout.setSpacing(3)
+        v_widget.setLayout(v_layout)
+        v_layout.addWidget(self.param_tree)
+        v_layout.addWidget(self.fit_btn)
+        v_layout.addWidget(self.add_btn)
+        v_layout.setContentsMargins(0,0,0,0)
+        v_widget.setContentsMargins(0,0,0,0)
+
+        self.plot_grid.set_shape(2,1)
+        self.plot_grid[0,0].setTitle('data')
+        self.plot_grid[1,0].setTitle('processing')
+
+        h_splitter.addWidget(v_widget)
+        h_splitter.addWidget(self.plot_grid)
+
+        self.processing_params = pg.parametertree.Parameter.create(name='Processing', type='group', children=[
+            {'name':'pre-deconvolve bessel', 'type':'float', 'value':6e3, 'suffix':'Hz', 'siPrefix':True, 'dec':True},
+            {'name':'deconvolve tau', 'type':'float', 'value':15e-3, 'suffix':'s', 'siPrefix':True, 'dec':True},
+            {'name':'post-deconvolve bessel', 'type':'float', 'value':1e3, 'suffix':'Hz', 'siPrefix':True, 'dec':True},
+            {'name':'event_threshold_fraction', 'type':'float', 'value':0.3, 'bounds':(0,1)}
+            ])
+        self.param_tree.addParameters(self.processing_params)
+        self.event_params = pg.parametertree.Parameter.create(name="Events", type='group')
+        self.param_tree.addParameters(self.event_params)
+        #self.event_params.sigAddNew.connect(self.add_event_param)
+
+        for param in self.processing_params.children():
+            param.sigValueChanged.connect(self.process_data)
+
+    def add_event_param(self, name, latency):
+        color = pg.intColor(len(self.event_params.children())+1)
+        param = pg.parametertree.Parameter.create(name=name, type='group', children=[
+            #{'name':'latency', 'type':'float', 'value':latency, 'suffix':'s', 'siPrefix':True},
+            LatencyParam(name='latency', value=latency, color=color),
+            {'name': 'display_color', 'type':'color', 'value':color, 'readOnly':True},
+            {'name': 'Fit event', 'type':'action'}
+            ])
+        self.event_params.addChild(param)
+        param.child('Fit event').sigActivated.connect(self.fit_event)
+        self.plot_grid[(0,0)].addItem(param.child('latency').line)
+
+    def fit_event(self, *args):
+        print('fit event called. args:%s' % args)
+
+    def update_events(self, events):
+        for ch in self.event_params.children():
+            self.plot_grid[(0,0)].removeItem(ch.child('latency').line)
+        self.event_params.clearChildren()
+
+        times = events['time']
+        zeroth = np.argwhere(times == times[times > 0].min())
+
+        for i, ev in enumerate(events):
+            self.add_event_param('event_%i'%(i-zeroth), ev['time'])
+
+
+
+    def plot_responses(self, responses):
+        self.responses = responses
+        qc_color = {'qc_pass': (255, 255, 255, 100), 'qc_fail': (255, 0, 0, 100)}
+        
+        for qc, prs in responses.items():
+            if len(prs) == 0:
+                continue
+            prl = PulseResponseList(prs)
+            post_ts = prl.post_tseries(align='pulse', bsub=True)
+            
+            for trace in post_ts:
+                item = self.plot_grid[(0,0)].plot(trace.time_values, trace.data, pen=qc_color[qc])
+                if qc == 'qc_fail':
+                    item.setZValue(-10)
+                #self.items.append(item)
+            if qc == 'qc_pass':
+                self.average_response = post_ts.mean()
+                item = self.plot_grid[(0,0)].plot(self.average_response.time_values, self.average_response.data, pen={'color': 'b', 'width': 2})
+                #self.items.append(item)
+        self.plot_grid[(0,0)].autoRange()
+        self.process_data()
+
+    def process_data(self):
+
+        plot = self.plot_grid[(1,0)]
+        plot.clear()
+
+        plot.plot(self.average_response.time_values, self.average_response.data)
+
+        pre_bessel = self.processing_params['pre-deconvolve bessel']
+        post_bessel = self.processing_params['post-deconvolve bessel']
+        tau = self.processing_params['deconvolve tau']
+        threshold_frac = self.processing_params['event_threshold_fraction']
+
+
+        pre_filtered = filters.bessel_filter(self.average_response, pre_bessel, order=4, btype='low', bidir=True)
+        deconvolved = ev_detect.exp_deconvolve(pre_filtered, tau)
+        post_filtered = filters.bessel_filter(deconvolved, post_bessel, order=1, btype='low', bidir=True)
+
+        plot.plot(pre_filtered.time_values, pre_filtered.data, pen='b')
+        plot.plot(deconvolved.time_values, deconvolved.data, pen='r')
+        plot.plot(post_filtered.time_values, post_filtered.data, pen='g')
+
+        threshold = threshold_frac*max(post_filtered.data)
+        plot.plot(post_filtered.time_values, [threshold]*len(post_filtered), pen='y')
+
+        ## index, length, sum, peak, peak_index, time, duration, area, peak_time
+        dec_events = ev_detect.threshold_events(post_filtered, threshold) ## find events in deconvolved trace
+        plot.addItem(pg.VTickGroup(dec_events['time']))
+
+        self.update_events(dec_events)
+
+        # dec_times = list(dec_events['time']) + [None]
+        # event_times = []
+        # for i in range(len(dec_events['time'])):
+        #     ev = deconvolved.time_slice(dec_times[i], dec_times[i+1])
+        #     d = np.concatenate([np.full((100), ev.data[0]), ev.data, np.full((int(0.1/ev.dt)), ev.data[-1])])
+        #     tv = np.concatenate([np.arange(ev.t0-ev.dt*100, ev.t0-ev.dt/10., ev.dt), ev.time_values, np.arange(ev.time_values[-1]+ev.dt, ev.time_values[-1]+0.1+ev.dt, ev.dt)])
+        #     data1 = TSeries(data=d, time_values=tv)
+        #     data = ev_detect.exp_reconvolve(data1, tau)
+        #     thresh = (max(data.data)-data.data[-1])*threshold_frac ## need to figure this out for negative going events too
+        #     event = ev_detect.threshold_events(data, thresh)
+        #     if len(event) > 1:
+        #         raise Exception('stop')
+        #     event_times.append(event['time'][0])
+
+
+        
+        # plot.addItem(pg.VTickGroup(event_times, (0.7, 1)))
+
+
+class LatencyParam(pg.parametertree.parameterTypes.SimpleParameter):
+
+    def __init__(self, *args, **kargs):
+        kargs.update({'siPrefix':True, 'suffix':'s', 'type':'float', 'step':100e-6})
+        pg.parametertree.parameterTypes.SimpleParameter.__init__(self, *args, **kargs)
+
+        self.line = pg.InfiniteLine(pos=self.value(), pen=kargs.get('color'), movable=True)
+
+        self.sigValueChanged.connect(self.update_line_position)
+        self.line.sigPositionChanged.connect(self.update_param_value)
+
+    def update_line_position(self, param):
+        with pg.SignalBlock(self.line.sigPositionChanged, self.update_param_value):
+            self.line.setValue(param.value())
+
+    def update_param_value(self, line):
+        with pg.SignalBlock(self.sigValueChanged, self.update_line_position):
+            self.setValue(line.value())
+
+
+
+
+
+
+
+
+
+
+
+
         
