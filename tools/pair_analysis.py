07673815b3cbaa78cf33ff5e274c956d6cc5e126
Megan Kratz <meganbkratz@gmail.com>
2020-10-02 16:52:49 -0400

    only do query for experiments if experiments were specified

--- tools/pair_analysis.py
+++ tools/pair_analysis.py
@@ -23,19 +23,21 @@ if __name__ == '__main__':
 
     default_session = db.session()
     notes_session = notes_db.db.session()
-    # timestamps = [r.acq_timestamp for r in db.query(db.Experiment.acq_timestamp).all()]
-    timestamps = []
     
     #mw = PairAnalysisWindow(default_session, notes_session)
     mw = OptoPairAnalysisWindow(default_session, notes_session)
+
+    # timestamps = [r.acq_timestamp for r in db.query(db.Experiment.acq_timestamp).all()]
+    timestamps = []
     if args.timestamps is not None:
         timestamps = args.timestamps
     elif args.expt_id is not None:
         timestamps = [args.expt_id]
-    
-    q = default_session.query(db.Experiment).filter(db.Experiment.acq_timestamp.in_(timestamps))
-    expts = q.all()
-    mw.set_expts(expts)
+
+    if timestamps != []:
+        q = default_session.query(db.Experiment).filter(db.Experiment.acq_timestamp.in_(timestamps))
+        expts = q.all()
+        mw.set_expts(expts)
 
     if None not in (args.expt_id, args.pre_cell_id, args.post_cell_id):
         expt = db.experiment_from_ext_id(args.expt_id)
