3fcc7126bdf8f3a390f63c2bb79bb196b7b6edcb
Megan Kratz <meganbkratz@gmail.com>
2020-10-02 15:35:40 -0400

    only do expt query if we have experiments

--- tools/pair_analysis.py
+++ tools/pair_analysis.py
@@ -27,22 +27,24 @@ if __name__ == '__main__':
 
     default_session = db.session()
     notes_session = notes_db.db.session()
-    # timestamps = [r.acq_timestamp for r in db.query(db.Experiment.acq_timestamp).all()]
-    timestamps = []
     prof.mark('got db sessions')
     
     #mw = PairAnalysisWindow(default_session, notes_session)
     mw = OptoPairAnalysisWindow(default_session, notes_session)
     prof.mark('created gui window')
+
+    # timestamps = [r.acq_timestamp for r in db.query(db.Experiment.acq_timestamp).all()]
+    timestamps = []
     if args.timestamps is not None:
         timestamps = args.timestamps
     elif args.expt_id is not None:
         timestamps = [args.expt_id]
-    
-    q = default_session.query(db.Experiment).filter(db.Experiment.acq_timestamp.in_(timestamps))
-    expts = q.all()
-    mw.set_expts(expts)
-    prof.mark('set experiments')
+
+    if timestamps != []:
+        q = default_session.query(db.Experiment).filter(db.Experiment.acq_timestamp.in_(timestamps))
+        expts = q.all()
+        mw.set_expts(expts)
+        prof.mark('set experiments')
 
     if None not in (args.expt_id, args.pre_cell_id, args.post_cell_id):
         expt = db.experiment_from_ext_id(args.expt_id)
